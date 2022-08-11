913a1728abbc9cb6aadfcb1cb9cd25a5f4877672
«Diana» <diana-mk@ya.ru>
2022-08-11 13:59:52 +0700

    сделали первую задачу

---
+++ task0/Program.cs
@@ -0,0 +1,3 @@
+﻿System.Console.WriteLine("Введите число: "); //приглашение к вводу
+int number = Convert.ToInt32(Console.ReadLine()); //считывание данных с консоли и прямое преобразование из строки в число
+System.Console.WriteLine(number * number); //вывод на экран number в квадрате
