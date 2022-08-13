dee64f08a62c74bd60cdf2e3e200e2e50313cde5
«Diana» <diana-mk@ya.ru>
2022-08-13 20:08:12 +0700

    Cделали задачи на втором семинаре

---
+++ task11/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Задача 11: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98 */
+int number = new Random().Next(100, 1000);
+System.Console.WriteLine($"Случайное число = {number}");
+int digit1 = number / 100;
+int digit3 = number % 10;
+// System.Console.Write(digit1);
+// System.Console.Write(digit3);
+System.Console.WriteLine($"Результат выполнения равен {digit1 * 10 + digit3}");
---
+++ task9/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 100);
+System.Console.WriteLine($"Случайное число = {number}");
+int digit2 = number % 10;
+int digit1 = number / 10;
+if (digit1 > digit2)
+{
+	System.Console.WriteLine($"Максимальная цифра числа первая и равна: {digit1}");
+}
+else
+{
+	System.Console.WriteLine($"Максимальная цифра числа вторая и равна: {digit2}");
+}
